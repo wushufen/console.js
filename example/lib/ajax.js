c69c9bc56b9fb2635a0cdf1f6c7d091c87383251
wushufen <wushufen@sunlands.com>
2018-06-20 17:54:58 +0800

    u ajax

--- example/lib/ajax.js
+++ example/lib/ajax.js
@@ -1,15 +1,76 @@
 /**
  * https://github.com/wusfen/ajax.js
- * wushufen 20171228~20180516
+ * wushufen 20171228~20180613
  */
-!(function() {
+!(function (window) {
     if (!window.XMLHttpRequest) {
-        window.XMLHttpRequest = function() {
+        window.XMLHttpRequest = function () {
             return new ActiveXObject("Microsoft.XMLHTTP")
         }
     }
 
-    var noop = function () {}
+    var noop = function () { }
+
+    var typeOf = function (obj) {
+        return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+    }
+
+    var extend = function (obj, _obj) {
+        for (var key in _obj) {
+            var val = obj[key]
+            var _val = _obj[key]
+            if (typeOf(_val) == 'object') {
+                obj[key] = extend(val ? val : {}, _val)
+            } else {
+                obj[key] = _obj[key]
+            }
+        }
+        return obj
+    }
+
+    var map = function (arr, fn) {
+        var list = []
+        for (var i = 0; i < arr.length; i++) {
+            list[i] = fn(arr[i], i)
+        }
+        return list
+    }
+
+    var serialize = function (value) {
+        var arr = [] // [ {keys, value}, ]
+
+        !(function loop(keys, value) {
+            if (typeof value != 'object') {
+                arr.push({ keys: keys, value: value })
+            } else {
+                for (var k in value) {
+                    loop(keys.concat(k), value[k]) // [k1,k2,..]
+                }
+            }
+        })([], value)
+
+        // [ 'obj[key][]=value', ]
+        arr = map(arr, function (item, i) {
+            var keyPath = ''
+            var keys = item.keys
+            map(keys, function (key, i) {
+                var _key = '[' + key + ']'
+                if (i == 0) {
+                    _key = key
+                }
+                var lastI = keys.length - 1
+                if (i == lastI && !isNaN(keys[lastI])) {
+                    _key = '[]'
+                }
+                keyPath += _key
+            })
+            // ecKeyPath=ecValue
+            return encodeURIComponent(keyPath) + (keyPath ? '=' : '') + encodeURIComponent(item.value)
+        })
+
+        // ...&...
+        return arr.join('&')
+    }
 
     var setting = {
         base: '',
@@ -21,30 +82,10 @@
         },
         async: true,
         cache: false,
-        mock: false,
         before: noop,
         after: noop,
         success: noop,
         error: noop
-    }
-
-    // webpack 中 typeof+module.exports 会报错
-    // https://github.com/webpack/webpack/issues/7318
-    var _typeof = function (obj) {
-        return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
-    }
-
-    var extend = function (obj, _obj) {
-        for(var key in _obj){
-            var val = obj[key]
-            var _val = _obj[key]
-            if (_typeof(_val) == 'object') {
-                obj[key] = extend(val?val:{}, _val)
-            }else{
-                obj[key] = _obj[key]
-            }
-        }
-        return obj
     }
 
     function ajax(options) {
@@ -60,12 +101,14 @@
 
         // data
         var data = options.data
-        var kvs = []
-        data._t_ = + new Date
-        for (var key in data) {
-            kvs.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        // raw: text||application/json
+        if (typeOf(data) == 'string') {
+            options.dataStr = data
         }
-        options.dataStr = kvs.join('&') // key=value&key2=value2
+        // form
+        else {
+            options.dataStr = serialize(data)
+        }
 
         // href
         options.href = options.url
@@ -73,7 +116,7 @@
             options.href = options.base + '/' + options.url
         }
         if (options.type == 'GET') {
-            options.href = options.href + '?' + options.dataStr
+            options.href = options.href + (options.href.match(/\?/) ? '&' : '?') + options.dataStr
         }
 
 
@@ -82,60 +125,68 @@
         ajax.xhr = xhr
 
         // handle
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             // console.log(xhr.readyState, xhr.status, xhr.responseText)
             if (xhr.readyState != 4) return
 
+            // res
+            var res = xhr.responseText
+            try { res = JSON.parse(res) } catch (e) { }
+            ajax.res = res
+
+            // after
+            if (options.after(xhr, options, res)) {
+                return
+            }
+
             // success
-            if (xhr.status == 0 || xhr.status == 200 || xhr.status == 304) {
-
-                // res
-                var res = xhr.responseText
-                try { res = JSON.parse(res) } catch (e) {}
-                ajax.res = res
-
-                // after
-                if(options.after(xhr, options, res)){
-                    return
-                }
+            // xhr.status == 0 || 
+            if (xhr.status == 200 || xhr.status == 304) {
                 options.success(res)
-
             }
             // error
             else {
-
-                // after
-                if (options.after(xhr, options, '')) {
-                    return
-                }
                 options.error(xhr)
-
             }
         }
 
-        // send
+        // open
         xhr.open(options.type, options.href, options.async)
-        for(var key in options.headers){
+
+        // headers
+        for (var key in options.headers) {
             xhr.setRequestHeader(key, options.headers[key])
         }
+
+        // no-cache
+        if (!options.cache) {
+            xhr.setRequestHeader('If-Modified-Since', 0)
+        }
+
+        // send
         xhr.send(options.type == 'POST' ? options.dataStr : null)
 
         return xhr
     }
 
     ajax.setting = setting
-    ajax.post = function(options) {
+    ajax.post = function (options) {
         options.type = 'POST'
         return ajax(options)
     }
-    ajax.get = function(options) {
+    ajax.get = function (options) {
         options.type = 'GET'
         return ajax(options)
     }
-    ajax.setUp = function (options) {
+    ajax.config = function (options) {
         return extend(setting, options)
     }
+    ajax.setUp = ajax.config
 
     // export
-    window.ajax = ajax
-})();
+    if (typeof module != 'undefined') {
+        module.exports = ajax
+    } else {
+        window.ajax = ajax
+    }
+})(window);
